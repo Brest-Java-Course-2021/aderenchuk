9ad90f59adb4110592021e9da780efb7631690b2
Artsiom Dzeranchuk <derenchuk2001@mail.ru>
2021-02-20 18:36:58 +0300

    SpringConfig

--- price-calc/src/main/java/com/epam/brest/Main.java
+++ price-calc/src/main/java/com/epam/brest/Main.java
@@ -6,6 +6,8 @@ import com.epam.brest.model.ReadData;
 import com.epam.brest.model.Status;
 import com.epam.brest.model.StatusType;
 import com.epam.brest.selector.PriceSelector;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -23,10 +25,15 @@ public class Main {
         // Price per kg
         // Result = d * pr1 + w * pr2
 
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+
+        FileReader fileReader = applicationContext.getBean(FileReader.class);
+        PriceSelector priceSelector = applicationContext.getBean(PriceSelector.class);
+
         Scanner scanner = new Scanner(System.in);
-        Status currentStatus = new ReadData();
+        Status currentStatus = new ReadData(priceSelector, fileReader, scanner);
         while (currentStatus.getType() != StatusType.EXIT) {
-            currentStatus = currentStatus.handle(scanner);
+            currentStatus = currentStatus.handle();
         }
     }
 
